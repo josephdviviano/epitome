2b2a20ad567eff9b160becaccba06cc8cf7f7107
Joseph Viviano <joseph.d.viviano@gmail.com>
2014-02-13 20:45:00 -0500

    fixed tshift pattern

--- BACKUP/rsync_mirror.sh
+++ BACKUP/rsync_mirror.sh
@@ -1,1 +1,2 @@
-sudo rsync -PRav --delete /srv/. admin@192.168.0.102:/volume1/NetBackup/.
+# turned off mirroring
+sudo rsync --rsh='ssh -p 45101' -PRav /srv/. grandvizier@192.168.0.102:/volume1/NetBackup/.
