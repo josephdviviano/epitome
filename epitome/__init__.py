a0fa2c3e5ffd69b6b9a659a3125804b10b1315f3
Joseph Viviano <joseph.d.viviano@gmail.com>
2014-05-20 17:37:14 -0400

    permissions woes

--- epitome/__init__.py
+++ epitome/__init__.py
@@ -1,22 +1,9 @@
 """
-###############################################################################
+The EPItome-xl pipeline. If called from the command line, epitome.interface is
+run. This code will generate a large number of scripts to be submitted to
+the sun grid engine running on your computer.
 
-                       _  _ ___
-                      |_ |_) | _|_  _  ._ _   _ __ \/ |  
-                      |_ |  _|_ |_ (_) | | | (/_   /\ |_ 
-
-Usage:
-
-    epitome list               -- returns a list of all available modules.
-    epitome check              -- returns installation paths.
-    epitome check <experiment> -- checks directory structure for a given 
-                                  experiment.
-    epitome <module> -help     -- returns usage for a single module.
-    epitome run                -- boots up the user interface (which prints
-                                  out a set of processing scripts).
-
-#######################################################################jdv2014#
-
+I'll add more here later, promise. joseph.d.viviao@gmail.com
 """
 ###############################################################################
 ####### NB: I have no idea what I am doing here, so I am copying numpy. #######
@@ -24,7 +11,9 @@ Usage:
 from . import interface
 from . import config
 from . import utilities
-from . import stats
 
 # accessible from epitome name-space, but not imported from epitome import *
-from builtins import os, sys
+if sys.version_info[0] >= 3:
+    from builtins import os, sys
+else:
+    from __builtin__ import os, sys
--- epitome/interface.py
+++ epitome/interface.py
@@ -1,3 +1,23 @@
+"""
+###############################################################################
+
+                       _  _ ___
+                      |_ |_) | _|_  _  ._ _   _ __ \/ |  
+                      |_ |  _|_ |_ (_) | | | (/_   /\ |_ 
+
+Usage:
+
+    epitome list               -- returns a list of all available modules.
+    epitome check              -- returns installation paths.
+    epitome check <experiment> -- checks directory structure for a given 
+                                  experiment.
+    epitome <module> -help     -- returns usage for a single module.
+    epitome run                -- boots up the user interface (which prints
+                                  out a set of processing scripts).
+
+#######################################################################jdv2014#
+
+"""
 #!/usr/bin/env python
 
 def print_modules():
