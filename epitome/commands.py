43c4e062b4c4368c50d8182732b047a53b2bc211
Joseph Viviano <joseph.d.viviano@gmail.com>
2014-05-27 16:16:43 -0400

    changed input to raw_input

--- epitome/commands.py
+++ epitome/commands.py
@@ -7,11 +7,74 @@ shell script reference passed in each call.
 
 def invalid_selection():
     print('Invalid selection.')
-    return None
+    return None, None
 
-def selector_list():
+def selector_int(output):
+    # have the user enter a number
+    option = raw_input('#: ')
 
-def selector_dict():
+    # ensure response is non-negative
+    if int(option) >= 0:
+        response = int(option)
+    else:
+        response, output = invalid_selection()
+
+    return response, output
+
+def selector_list(item_list, output):
+
+    if type(item_list) != list:
+        raise TypeError('Input must be a list!')
+
+    # sort the input list
+    item_list.sort()
+
+    # print the options, and their numbers
+    for i, item in enumerate(item_list):
+        print(str(i+1) +': ' + str(item))
+
+    # retrieve the option number
+    option = raw_input('option #: ')
+
+    # test the response, response & output = Null if the user makes an error
+    try:
+        response = item_list[int(option)-1]
+    except:
+        response, output = invalid_selection()
+    if int(option) == 0:
+        response, output = invalid_selection()
+
+    return response, output
+
+def selector_dict(item_dict, output):
+    
+    if type(item_dict) != dict:
+        raise TypeError('Input must be a dict!')
+
+    # init list where we store / find the responses
+    item_list = []
+    
+    # generate a sorted list of dict keys
+    for item in item_dict:
+        item_list.append(item)
+    item_list.sort()
+
+    # loop through sorted list
+    for i, item in enumerate(item_list):
+        print(str(i+1) + ': ' + item + ' ' + item_dict[item])
+    
+    # retrieve the option number
+    option = raw_input('option #: ')
+
+    # test the response, response & output = Null if the user makes an error
+    try:
+        response = item_list[int(option)-1]
+    except:
+        response, output = invalid_selection()
+    if int(option) == 0: 
+        response, output = invalid_selection()
+
+    return response, output
 
 def filter(input_name):
     print('')
@@ -34,28 +97,12 @@ def init_EPI():
     # get the data-quality option
     print('Select data quality:')
     data_quality = ['low', 'high']
-    for i, qual in enumerate(data_quality):
-        print(str(i+1) +': ') + str(qual)
-    response = input('option #: ')
-
-    # test the response
-    try:
-        quality = data_quality[int(response-1)]
-    except:
-        output = invalid_selection()
-    if response == 0:
-        output = invalid_selection()
+    quality, output = selector_list(data_quality, output)
 
     # get the number of TRs to delete
     print('')
     print('Number of TRs to delete:')
-    response = input('delete TRs: ')
-
-    # test the response
-    if int(response) >= 0:
-        deltr = int(response)
-    else:
-        output = invalid_selection()
+    deltr, output = selector_int(output)
 
     # get the slice timing
     print('')
@@ -66,22 +113,7 @@ def init_EPI():
                   'alt-z2' : '= alternating, starting at slice #nz-2', 
                   'seq+z' : '= sequential in the plus direction',
                   'seq-z' : '= sequential in the minus direction'}
-
-    i = 0
-    pattern_list = []
-    for pattern in t_patterns:
-        pattern_list.append(pattern)
-        print(str(i+1) + ': ' + pattern + ' ' + t_patterns[pattern])
-        i = i + 1 # iterate
-    response = input('option #: ')
-
-    # test the response
-    try:
-        slice_timing = pattern_list[int(response-1)]
-    except:
-        output = invalid_selection()
-    if response == 0: 
-        output = invalid_selection()
+    slice_timing, output = selector_dict(t_patterns, output)
 
     # if we messed any of these up, we return None
     if output == None:
@@ -110,6 +142,8 @@ def linreg_FS2MNI_FSL(input_name):
     return line, output
 def linreg_calc_AFNI(input_name):
 
+    import copy
+
     # init our output variable, which will be returned unharmed if we don't 
     # make any mistakes
     output = copy.copy(input_name)
@@ -122,17 +156,7 @@ def linreg_calc_AFNI(input_name):
     # get the data-quality option
     print('Select data quality:')
     data_quality = ['low', 'high']
-    for i, qual in enumerate(data_quality):
-        print(str(i+1) + ': ' + str(qual))
-    response = input('option #: ')
-
-    # test the response
-    try:
-        quality = data_quality[int(response-1)]
-    except:
-        output = invalid_selection()
-    if response == 0:
-        output = invalid_selection()
+    quality, output = selector_list(data_quality, output)
 
     # set the cost function option
     print('')
@@ -152,40 +176,13 @@ def linreg_calc_AFNI(input_name):
                  'lpc+' : '= Local Pearson Signed + Others',
                  'ncd' : '= Normalized Compression Distance',
                  'lpc+zz' : '= Local Pearson Correlation Signed + Magic'}
-    
-    i = 0
-    cost_list = []
-    for cost in cost_fxns:
-        cost_list.append(cost)
-        print(str(i+1) + ': ' + cost + ' ' + cost_fxns[cost])
-        i = i + 1 # iterate
-
-    response = input('Cost function: ')
-
-    # test the response
-    try:
-        cost = cost_list[int(response-1)]
-    except:
-        output = invalid_selection()
-    if response == 0: 
-        output = invalid_selection()
+    cost, output = selector_dict(cost_fxns, output)
 
     # get registration degrees of freedom
     print('')
     print('Registration degrees of freedom: (see AFNI 3dTshift for help)')
     degrees_of_freedom = ['big_move', 'giant_move']
-
-    for i, reg_dof in enumerate(degrees_of_freedom):
-        print(str(i+1) + ': ' + str(reg_dof))
-    response = input('option #: ')
-
-    # test the response
-    try:
-        reg_dof = degrees_of_freedom[int(response-1)]
-    except:
-        output = invalid_selection()
-    if response == 0: 
-        output = invalid_selection()
+    reg_dof, output = selector_list(degrees_of_freedom, output)
 
     # if we messed any of these up, we return None
     if output == None:
@@ -198,8 +195,7 @@ def linreg_calc_AFNI(input_name):
                                                    str(cost) + ' ' +
                                                    str(reg_dof))
 
-    # we return the output given, because we did not operate directly on the
-    # functional data
+    # did not operate directly on the functional data, so return orig. output
     return line, output
 
 def linreg_calc_FSL(input_name):
