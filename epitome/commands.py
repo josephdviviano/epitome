889e447cf3bbc3e7af514027d7ffdd7fe848ed8c
josephdviviano <joseph.d.viviano@gmail.com>
2014-07-22 21:11:33 -0400

    debugging QC modules

--- epitome/commands.py
+++ epitome/commands.py
@@ -584,7 +584,7 @@ def check_runs(dir_data, expt, mode):
     print('\nAdding NIFTI dimension-checking QC to the outputs.')
 
     line = ('echo bash ${DIR_PIPE}/epitome/modules/qc/check_runs ' + 
-             str(dir_data) + ' ' + str(expt) + ' ' + str(mode))
+             str(dir_data) + ' ' + str(expt))
 
     return line, output
 
