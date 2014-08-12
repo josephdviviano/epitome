08560039bf6f09b5e8be341a3e39be50d0a42573
josephdviviano <joseph.d.viviano@gmail.com>
2014-08-12 00:13:27 -0400

    added ICA module

--- epitome/commands.py
+++ epitome/commands.py
@@ -189,6 +189,27 @@ def gen_regressors(input_name):
 
     return line, output
 
+def ICA(input_name):
+    output = copy.copy(input_name) # return output unharmed
+
+     print('\nAdding MELODIC ICA calculation')
+
+    try:
+        # masking
+        print('\nInput mask prefix (default = EPI_mask):')
+        mask_prefix = raw_input('Mask Prefix: ')
+        if mask_prefix == '':
+            mask_prefix = 'EPI_mask'
+    # if we messed any of these up, we return None
+    except ValueError as ve:
+        return '', None
+
+    # otherwise we print the command and return it
+    line = ('. ${DIR_PIPE}/epitome/modules/pre/ICA ' +
+                                      str(input_name) + ' ' +
+                                      str(mask_prefix)
+    return line, output
+
 def init_EPI():    
     output = 'scaled'
 
