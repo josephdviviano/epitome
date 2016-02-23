5964cfa9a9d530f468c78dfc6c78ed904ef25fe4
Erin Dickie <erin.dickie@camh.ca>
2016-02-23 13:08:00 -0500

    notes for making a hcp qc page

---
+++ epitome/wip/qc-fs2hcp.py
@@ -0,0 +1,7 @@
+## to run qc
+## copy qc_template.scene into your hcp directory as qc_views.scene
+## for each subject
+#     find and replace HCP_DATA_PATH/ with the $HCP_DATA directory path
+#     find and replace SUBJID with the subjects id
+#     run: wb_command -show-scene qcviews.scene 1 qc/subject/scene_file_test1_1.png 600 400
+## repeat above up to pic5
